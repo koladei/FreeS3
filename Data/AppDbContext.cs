76d6a5a3e29cce08a1fbe000e9a386a10deee1a2
Kolade Oluwagbenga Ige <kolade.ige@gmail.com>
2026-04-24 23:45:18 +0100

    Add AppDbContext and initial migration setup for token management

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CradleSoft.DMS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CradleSoft.DMS.Data
+{
+  public class AppDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+      
+    }
+
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    public DbSet<AccessToken> RevokedAccessTokens { get; set; }
+  }
+}
